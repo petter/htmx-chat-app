df491e1cab672f892c80cffd997f17903b62ca00
Petter Sæther Moen <pettersmoen@gmail.com>
2024-04-08 14:49:51 +0200

    init

---
+++ src/layouts/root.tsx
@@ -0,0 +1,18 @@
+import styles from "./index.css?url";
+
+export function RootLayout({
+  children,
+}: {
+  children: JSX.Element | Iterable<JSX.Element>;
+}) {
+  return (
+    <html>
+      <head>
+        <title>My App</title>
+        <link rel="stylesheet" href={styles} />
+        <script src="https://unpkg.com/htmx.org@1.9.11"></script>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
